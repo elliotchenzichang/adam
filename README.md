a44f75ce6d2a759d6b1b3eb2942ec9f8c84d45d6
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2024-08-05 23:15:56 +0800

    write down what I want to do with this project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # adam
-Help you generate the expected test data.
+Adam is a Golang API designed to streamline test data management. It simplifies the process of creating, modifying, and organizing test cases, especially for functions with intricate parameter structures. By automating routine data tasks, Adam empowers developers to focus on writing robust and effective unit tests.
--- adam.go
+++ adam.go
@@ -4,7 +4,19 @@ type Adam struct {
 	cases map[string]*Cas
 }
 
-func (a *Adam) AddCases(name string, c *Cas) error {
+func (a *Adam) AddCase(name string, c *Cas) error {
 	a.cases[name] = c
 	return nil
+}
+
+func (a *Adam) DeleteCase(name string) error {
+	return nil
+}
+
+func (a *Adam) UpdateCase(name string, c *Cas) error {
+	return nil
+}
+
+func (a *Adam) GetCase(name string) (*Cas, error) {
+	return nil, nil
 }
